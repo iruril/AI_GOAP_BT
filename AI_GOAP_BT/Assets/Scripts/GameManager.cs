a45078701975d4c9f75477d1845eb256b53c43fe
Tae Wook Han <31647650+iruril@users.noreply.github.com>
2025-11-21 08:53:47 +0900

    [Mod] 폴더 정리

--- AI_GOAP_BT/Assets/Scripts/GameManager.cs
+++ AI_GOAP_BT/Assets/Scripts/GameManager.cs
@@ -1,16 +1,6 @@
-using System;
 using UnityEngine;
 using Newtonsoft.Json;
 using System.Collections.Generic;
-
-public static class ConnectionTokenUtils
-{
-    public static byte[] NewToken() => Guid.NewGuid().ToByteArray();
-
-    public static int HashToken(byte[] token) => new Guid(token).GetHashCode();
-
-    public static string TokenToString(byte[] token) => new Guid(token).ToString();
-}
 
 public class GameManager : MonoBehaviour
 {
@@ -31,7 +21,7 @@ public class GameManager : MonoBehaviour
     {
         if (_connectionToken == null)
         {
-            _connectionToken = ConnectionTokenUtils.NewToken();
+            _connectionToken = TokenUtility.NewToken();
         }
 
         if (Instance == null)
---
+++ AI_GOAP_BT/Assets/Scripts/Utility/TokenUtility.cs
@@ -0,0 +1,9 @@
+using System;
+public static class TokenUtility
+{
+    public static byte[] NewToken() => Guid.NewGuid().ToByteArray();
+
+    public static int HashToken(byte[] token) => new Guid(token).GetHashCode();
+
+    public static string TokenToString(byte[] token) => new Guid(token).ToString();
+}
