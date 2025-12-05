ed16b5f18abe521e9507efb61227e1249f628b8a
Tae Wook Han <31647650+iruril@users.noreply.github.com>
2025-12-05 20:17:31 +0900

    [Add] 사선이 방해받았는지 검출하는 Conditional 노드 작성 및 적용

--- AI_GOAP_BT/Assets/Scripts/Actor/Assault/BT/Conditional/Assault_Conditional.cs
+++ AI_GOAP_BT/Assets/Scripts/Actor/Assault/BT/Conditional/Assault_Conditional.cs
@@ -1,5 +1,6 @@
 using BehaviorDesigner.Runtime.Tasks;
 using GOAP.Assualt;
+using UnityEngine;
 
 [TaskCategory("Shooter AI/Engage")]
 public class TargetInvisible : Conditional
@@ -20,6 +21,55 @@ public class TargetInvisible : Conditional
         else
         {
             return TaskStatus.Failure;
+        }
+    }
+}
+
+[TaskCategory("Shooter AI/Engage")]
+public class IsFriendlyInLOF : Conditional
+{
+    AssaultBrain brain;
+    RaycastHit[] buffer = new RaycastHit[2];
+    int result;
+
+    public override void OnAwake()
+    {
+        brain = GetComponent<AssaultBrain>();
+    }
+
+    public override void OnFixedUpdate()
+    {
+        base.OnFixedUpdate(); 
+        CheckLOF();
+    }
+
+    public override TaskStatus OnUpdate()
+    {
+        if (brain.Sensor.HasTarget && result > 0)
+        {
+            return TaskStatus.Success;
+        }
+        else
+        {
+            return TaskStatus.Failure;
+        }
+    }
+
+    void CheckLOF()
+    {
+        Vector3 origin = brain.Sensor.MyEyes.position;
+        Vector3 direction = brain.Sensor.LastSeenPosition - brain.Sensor.MyEyes.position;
+        float distance = direction.magnitude;
+        direction.Normalize();
+
+        int mask = WorldManager.Instance.GetLevelLayers() | (1 << gameObject.layer);
+        int hitCount = Physics.SphereCastNonAlloc(origin, 0.1f, direction, buffer, distance, mask);
+
+        result = 0;
+        for (int i = 0; i < hitCount; i++)
+        {
+            if (buffer[i].collider.gameObject != this.gameObject)
+                result++;
         }
     }
 }
